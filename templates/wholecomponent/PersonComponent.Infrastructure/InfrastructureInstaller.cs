cfec143d498bf0063dbd3b59c82d2706a47bfd20
Mikołaj Dalecki <dalecki.mikolaj@gmail.com>
2021-09-18 15:41:47 +0200

    First idea of my own template.

---
+++ templates/wholecomponent/PersonComponent.Infrastructure/InfrastructureInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PersonComponent.Core
+{
+    public static class InfrastructureInstaller
+    {
+        public static void InstallPersonComponentCore(this IServiceCollection services)
+        {
+            // Here add your services from core
+            // services.AddTransient<IPersonService, PersonService>();
+            // services.AddTransient<IValidator<PersonModel>, PersonValidator>();
+        }
+    }
+}
