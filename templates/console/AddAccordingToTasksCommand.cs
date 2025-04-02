895b226a11642ae18d56b8be493d27c0e07cfe41
Mikołaj Dalecki <mikdal_opensource@outlook.com>
2025-04-02 23:38:12 +0200

    Updated console project

---
+++ templates/console/AddAccordingToTasksCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+using System.Threading.Tasks;
+using System.CommandLine.NamingConventionBinder;
+using System.IO;
+
+namespace ConsoleTemplate;
+
+class AddAccordingToTasksCommand : Command
+{
+    public AddAccordingToTasksCommand() : base("commandName", "Command Description")
+    {
+        AddOption(new Option<int?>("--limit", () => 4, "Quite long description."));
+        AddOption(new Option<DirectoryInfo>("--directory", "desc") { IsRequired = true });
+
+        this.Handler = CommandHandler.Create(this.Execute);
+    }
+
+    public Task<int> Execute(int? limit, DirectoryInfo katalogZdjec, int startowyEan)
+    {
+        return Task.FromResult(0);
+    }
+}
--- templates/console/Program.cs
+++ templates/console/Program.cs
@@ -2,31 +2,22 @@
 using System.CommandLine;
 using System.Threading;
 using System.Threading.Tasks;
-using System.CommandLine.Invocation;
 
-namespace ConsoleTemplate
+namespace ConsoleTemplate;
+
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var anIntOption = new Option<int>("--an-int", getDefaultValue: () => 42, description: "Description 1");
-            var aStringOption = new Option<string>("--a-string");
+        var rootCommand = new RootCommand();
+        rootCommand.Description = "<<Application description>>";
+        rootCommand.AddCommand(new AddAccordingToTasksCommand());
 
-            var rootCommand = new RootCommand()
-            {
-                anIntOption, aStringOption,
-                new Command("subverb")
-            };
-            rootCommand.Description = "Application description";
-            rootCommand.SetHandler((int myInt, string myString, CancellationToken token) => DoSomething(myInt, myString, token), anIntOption, aStringOption);
+        await rootCommand.InvokeAsync(args);
+    }
 
-            await rootCommand.InvokeAsync(args);
-        }
-
-        public static void DoSomething(int anInt, string aString, CancellationToken token)
-        {
-            Console.WriteLine("Hello world!");
-        }
+    public static void DoSomething(int anInt, string aString, CancellationToken token)
+    {
+        Console.WriteLine("Hello world!");
     }
 }
