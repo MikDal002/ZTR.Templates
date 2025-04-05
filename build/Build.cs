1332555d071cd82a225ba98ed0682e5ab199cccf
Mikołaj Dalecki <mikdal_opensource@outlook.com>
2025-04-05 15:56:13 +0200

    Dodano podstawy budowania projektu i publikowania go do katalogu

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Serilog;
 //using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
@@ -19,26 +16,90 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.ZipDirectory);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution] readonly Solution Solution;
+    [GitVersion] readonly GitVersion GitVersion;
+
+
+    const string ProjectToBePublished = "ConsoleTemplate";
+    AbsolutePath PublishedProjectAsZip =>
+        PackagesDirectory / ProjectToBePublished + ".zip";
+    AbsolutePath SourceDirectory => RootDirectory / "templates";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath PackagesDirectory => RootDirectory / "packages";
+    AbsolutePath TestResultDirectory => RootDirectory / "testResults";
+
     Target Clean => _ => _
         .Before(Restore)
+        .Unlisted()
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(d => d.DeleteDirectory());
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetTasks.DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            Log.Information($"Compiling {GitVersion.SemVer} version");
+
+            DotNetTasks.DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
         });
+
+    Target Publish => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            OutputDirectory.CreateOrCleanDirectory();
+
+
+            var projectToPublish = Solution.GetProject(ProjectToBePublished);
+            Log.Information("Publishing {projectToPublish} project to {filePath} directory.", projectToPublish,
+                OutputDirectory);
+
+            DotNetTasks.DotNetPublish(s => s.SetProject(projectToPublish)
+                .SetConfiguration(Configuration)
+                .SetOutput(OutputDirectory)
+                .SetNoBuild(true));
+        });
+
+    Target ZipDirectory => _ => _
+        .DependsOn(Publish)
+        .Executes(() =>
+        {
+            OutputDirectory
+                .ZipTo(PublishedProjectAsZip,
+                    compressionLevel: System.IO.Compression.CompressionLevel.SmallestSize,
+                    fileMode: System.IO.FileMode.Create);
+        });
+
+    Target Tests => _ => _
+       .DependsOn(Compile)
+       .TriggeredBy(Compile)
+       .Executes(() =>
+       {
+           TestResultDirectory.CreateOrCleanDirectory();
+           DotNetTasks.DotNetTest(s => s.SetConfiguration(Configuration)
+               .SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
+               .EnableNoBuild()
+               .SetProjectFile(Solution));
+       });
 
 }
