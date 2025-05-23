51a7ecec5494d3cbf515fd13c53d3384309c2eb5
Our Company Build <build@ourcompany.com>
2025-05-23 15:55:46 +0200

    ♻️ refactor(build): consolidate on Velopack GitHub releases workflow
    disable previous generic GitHub release creation in favor of Velopack flow
    remove legacy Velopack SSH deployment scripts
    simplify PublishToGithub target conditions

--- build/Build.Velopack.PublishToSsh.cs
+++ build/Build.Velopack.PublishToSsh.cs
@@ -1,50 +1,24 @@
-﻿using Nuke.Common;
+using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities;
 using Renci.SshNet;
 
 partial class Build
 {
-    [PathVariable] public Tool Vpk;
     [PathVariable] public Tool Scp;
     [PathVariable] public Tool Ssh;
+
+    [Parameter] readonly int MaxReleasesOnServer = 2;
+    [Parameter] readonly string DirectoryForReleases = "releases";
 
     [Parameter] readonly int SshPort = 10366;
     [Parameter] readonly string SshUser = "frog";
     [Parameter] readonly string SshServer = "frog02.mikr.us";
     ConnectionInfo SshConnectionInfo => new(SshServer, port: SshPort, SshUser, new PrivateKeyAuthenticationMethod(SshUser, new PrivateKeyFile(SshPrivateKey)));
 
-    [Parameter] readonly int MaxReleasesOnServer = 2;
-    [Parameter] readonly string DirectoryForReleases = "releases";
-    string Channel => $"{OperationSystem}-{SystemArchitecture}" + (GitVersion.PreReleaseLabel.IsNullOrWhiteSpace() ? "" : "-alpha");
-
     AbsolutePath SshPrivateKey = RootDirectory / "ztrtemplates";
-
-    AbsolutePath VelopackDirectory = RootDirectory / "Velopack";
     AbsolutePath VelopackReleaseMirroredFromRemoteServer => VelopackDirectory / DirectoryForReleases;
-    AbsolutePath VelopackPublish => VelopackDirectory / "publish";
     string NameProjectDirectoryOnTheServer => NameOfProjectToBePublished.ToLower();
-
-    Target CleanVelopack => _ => _
-        .TriggeredBy(Clean)
-        .Executes(() =>
-        {
-            VelopackDirectory.DeleteDirectory();
-        });
-
-    Target PackWithVelopack => _ => _
-        .DependsOn(ConfigureAppSettings)
-        .DependsOn(CleanVelopack)
-        .Executes(() =>
-        {
-            var iconPath = ProjectToPublish.Directory / "applogo.ico";
-            Assert.True(iconPath.FileExists());
-
-            Vpk.Invoke($@"vpk [{OperationSystem}] pack -u {NameOfProjectToBePublished} -v {GitVersion.FullSemVer}" +
-                       $@" -p {PublishDirectory} --icon {iconPath} --outputDir {VelopackPublish}" +
-                       $@" --runtime {OperationSystem}-{SystemArchitecture} --channel {Channel} --delta none");
-        });
 
     Target DownloadServerToLocal => _ => _
         .Executes(() =>
@@ -61,15 +35,11 @@ partial class Build
         });
 
     Target UploadLocalToServer => _ => _
-        // .DependsOn(PackWithVelopack) // This target should likely depend on PackWithVelopack
-        .DependsOn(ConfigureAppSettings) // Ensure settings are configured before packing
-        .DependsOn(PackWithVelopack) // Ensure packing happens after configuration
-        .DependsOn(DownloadServerToLocal) // Keep existing dependency
+        .DependsOn(ConfigureAppSettings)
+        .DependsOn(PackWithVelopack)
+        .DependsOn(DownloadServerToLocal)
         .Executes(() =>
         {
-            // Note: The original PackWithVelopack target was modified to depend on ConfigureAppSettings.
-            // This UploadLocalToServer target likely needs to run *after* PackWithVelopack.
-            // Let's adjust PackWithVelopack dependency instead.
 
             Vpk.Invoke($"vpk upload local --path {VelopackReleaseMirroredFromRemoteServer} --channel {Channel} --keepMaxReleases {MaxReleasesOnServer} --outputDir {VelopackPublish} --regenerate");
             Scp.Invoke($"-i {SshPrivateKey} -r -P {SshPort} {VelopackReleaseMirroredFromRemoteServer} {SshUser}@{SshServer}:/var/www/html/{NameProjectDirectoryOnTheServer}/");
