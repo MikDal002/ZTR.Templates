5c0ce0c1f0c9da200958a55bcd7f2f441c34a74c
MikDal002 <MikDal002@users.noreply.github.com>
2026-05-22 22:38:29 +0000

    deploy: 7b1f030785f5bb18e94e800a46272a9654ff4969

---
+++ pr-60/assets/js/acecf23e.992c7895.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/ZtrTemplates/pr-60/blog","blogTitle":"Blog","authorsListPath":"/ZtrTemplates/pr-60/blog/authors"}')}}]);
