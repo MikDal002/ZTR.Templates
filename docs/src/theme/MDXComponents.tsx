1843c93e7de230c9f6f7fcb2af6fa4627fb9ffc0
MikDal002 <mikdal_opensource@outlook.com>
2026-07-07 18:25:36 +0000

    feat: ✨ Add Docusaurus and PR title validation documentation; restructure Husky.Net setup

---
+++ docs/src/theme/MDXComponents.tsx
@@ -0,0 +1,8 @@
+ import MDXComponents from '@theme-original/MDXComponents';
+     import {TemplateFeatureFiles, TemplateFeatureWebsite} from '@site/src/components/TemplateFeature';
+
+     export default {
+       ...MDXComponents,
+       TemplateFeatureFiles,
+       TemplateFeatureWebsite,
+     };
